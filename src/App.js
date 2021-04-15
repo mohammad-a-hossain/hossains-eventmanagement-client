063b203d514d00a6d61ab91834f8c266d8b4618f
m-a-hossain <adam.eve007@gmail.com>
2021-04-15 19:18:41 +0600

    after big error

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+//import Login from './components/login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <h1>lets start work</h1>
+   {/*  <Login></Login> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/Login.js
@@ -0,0 +1,18 @@
+/* import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>login page</h1>
+
+     <form>
+         <input type="email" name="email" placeholder='enter your name'/>
+         <input type="password" name="pass"placeholder='enter your password'/>
+             <button type='submit' value='Submit'>submit</button>
+     </form>
+    </div>
+  )
+}
+
+export default Login
+ */
